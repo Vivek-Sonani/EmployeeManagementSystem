77adb015ad6afa45ddecca600d3e03b9b3d368d7
vivek sonani <viveks@codetailor.in>
2024-01-13 14:53:40 +0530

    Fix: remove unused dependency

--- src/main/java/com/springboot/employee/service/EmployeeServiceImp.java
+++ src/main/java/com/springboot/employee/service/EmployeeServiceImp.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -48,9 +47,6 @@ public class EmployeeServiceImp implements EmployeeService {
 
 	@Autowired
 	private AddressService addressService;
-
-	@Autowired
-	private FamilyInfoService familyInfoService;
 
 	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
