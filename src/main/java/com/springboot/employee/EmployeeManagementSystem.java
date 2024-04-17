2602659bc3c6ddbc13a822a3cb77497e145f0ab7
Vivek Sonani <viveks@codetailor.in>
2024-04-17 11:34:15 +0530

    Update

--- src/main/java/com/springboot/employee/EmployeeManagementSystem.java
+++ src/main/java/com/springboot/employee/EmployeeManagementSystem.java
@@ -15,36 +15,36 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableJpaRepositories
 @EnableJpaAuditing
-//@EnableGlobalMethodSecurity  //-->  for @preAuthorize annotation
+// @EnableGlobalMethodSecurity //--> for @preAuthorize annotation
 public class EmployeeManagementSystem {
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementSystem.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeeManagementSystem.class, args);
+  }
 }
-//	@Bean
-//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//		return args -> {
-//			System.out.println("Let's inspect the beans provided by Spring Boot:");
-//			String[] beanNames =  ((ListableBeanFactory) ctx).getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
-//		};
-//	}
-//	@Bean
-//	public boolean createTestIndex(RestHighLevelClient restHighLevelClient) throws Exception {
-//		try {
-//			DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("hello-world");
-//			restHighLevelClient.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT); // 1
-//		} catch (Exception ignored) {
-//		}
+// @Bean
+// public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+// return args -> {
+// System.out.println("Let's inspect the beans provided by Spring Boot:");
+// String[] beanNames = ((ListableBeanFactory) ctx).getBeanDefinitionNames();
+// Arrays.sort(beanNames);
+// for (String beanName : beanNames) {
+// System.out.println(beanName);
+// }
+// };
+// }
+// @Bean
+// public boolean createTestIndex(RestHighLevelClient restHighLevelClient) throws Exception {
+// try {
+// DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("hello-world");
+// restHighLevelClient.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT); // 1
+// } catch (Exception ignored) {
+// }
 //
-//		CreateIndexRequest createIndexRequest = new CreateIndexRequest("hello-world");
-//		createIndexRequest.settings(
-//				Settings.builder().put("index.number_of_shards", 1)
-//						.put("index.number_of_replicas", 0));
-//		restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT); // 2
+// CreateIndexRequest createIndexRequest = new CreateIndexRequest("hello-world");
+// createIndexRequest.settings(
+// Settings.builder().put("index.number_of_shards", 1)
+// .put("index.number_of_replicas", 0));
+// restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT); // 2
 //
-//		return true;
-//	}
+// return true;
+// }
