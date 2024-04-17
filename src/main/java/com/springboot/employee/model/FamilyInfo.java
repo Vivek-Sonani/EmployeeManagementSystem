edf7ac2d701f6ae9d930eadb0ebd4ea51d63595a
Vivek Sonani <viveks@codetailor.in>
2024-04-17 17:35:22 +0530

    Fix

--- src/main/java/com/springboot/employee/model/FamilyInfo.java
+++ src/main/java/com/springboot/employee/model/FamilyInfo.java
@@ -27,128 +27,129 @@ import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.fasterxml.jackson.annotation.OptBoolean;
 
 @Entity
-@EntityListeners(value = { AuditListner.class, AuditingEntityListener.class })
+@EntityListeners(value = {AuditListner.class, AuditingEntityListener.class})
 @Table(name = "family_info")
 public class FamilyInfo {
-	// define enum Relationship because number of fields are constant.
-	enum Relationship {
-		FATHER, MOTHER, BROTHER
-	}
+  // define enum Relationship because number of fields are constant.
+  enum Relationship {
+    FATHER, MOTHER, BROTHER
+  }
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 
-	@Column
-	private String name;
+  @Column
+  private String name;
 
-	@Column
-	@Enumerated(EnumType.STRING)
-	private Relationship relationship;
+  @Column
+  @Enumerated(EnumType.STRING)
+  private Relationship relationship;
 
-	@Column
-	@JsonFormat(shape = Shape.STRING, pattern = "dd-MM-yyyy", lenient = OptBoolean.TRUE)
-	private Date dob;
+  @Column
+  @JsonFormat(shape = Shape.STRING, pattern = "dd-MM-yyyy", lenient = OptBoolean.TRUE)
+  private Date dob;
 
-	@Column
-	private String occupation;
+  @Column
+  private String occupation;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "employee_id")
-	@JsonBackReference
-	private Employee employee;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "employee_id")
+  @JsonBackReference
+  private Employee employee;
 
-	@Column(name = "created_date", updatable = false)
-	@CreatedDate
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date createdDate;
+  @Column(name = "created_date", updatable = false)
+  @CreatedDate
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
 
-	@Column(name = "modified_date")
-	@LastModifiedDate
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date modifiedDate;
+  @Column(name = "modified_date")
+  @LastModifiedDate
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date modifiedDate;
 
-	public Employee getEmployee() {
-		return employee;
-	}
+  public Employee getEmployee() {
+    return employee;
+  }
 
-	public void setEmployee(Employee employee) {
-		this.employee = employee;
-	}
+  public void setEmployee(Employee employee) {
+    this.employee = employee;
+  }
 
-	public FamilyInfo() {
-		super();
-	}
+  public FamilyInfo() {
+    super();
+  }
 
-	public FamilyInfo(Integer id, String name, Relationship relationship, Date dob, String occupation) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.relationship = relationship;
-		this.dob = dob;
-		this.occupation = occupation;
-	}
+  public FamilyInfo(Integer id, String name, Relationship relationship, Date dob,
+      String occupation) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.relationship = relationship;
+    this.dob = dob;
+    this.occupation = occupation;
+  }
 
-	public Integer getId() {
-		return id;
-	}
+  public Integer getId() {
+    return id;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public Relationship getRelationship() {
-		return relationship;
-	}
+  public Relationship getRelationship() {
+    return relationship;
+  }
 
-	public Date getDob() {
-		return dob;
-	}
+  public Date getDob() {
+    return dob;
+  }
 
-	public String getOccupation() {
-		return occupation;
-	}
+  public String getOccupation() {
+    return occupation;
+  }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public void setRelationship(Relationship relationship) {
-		this.relationship = relationship;
-	}
+  public void setRelationship(Relationship relationship) {
+    this.relationship = relationship;
+  }
 
-	public void setDob(Date dob) {
-		this.dob = dob;
-	}
+  public void setDob(Date dob) {
+    this.dob = dob;
+  }
 
-	public void setOccupation(String occupation) {
-		this.occupation = occupation;
-	}
+  public void setOccupation(String occupation) {
+    this.occupation = occupation;
+  }
 
-	public Date getCreatedDate() {
-		return createdDate;
-	}
+  public Date getCreatedDate() {
+    return createdDate;
+  }
 
-	public Date getModifiedDate() {
-		return modifiedDate;
-	}
+  public Date getModifiedDate() {
+    return modifiedDate;
+  }
 
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
 
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
+  public void setModifiedDate(Date modifiedDate) {
+    this.modifiedDate = modifiedDate;
+  }
 
-	@Override
-	public String toString() {
-		return "FamilyInfo [id=" + id + ", name=" + name + ", relationship=" + relationship + ", dob=" + dob
-				+ ", occupation=" + occupation + ", employee=" + employee + "]";
-	}
+  @Override
+  public String toString() {
+    return "FamilyInfo [id=" + id + ", name=" + name + ", relationship=" + relationship + ", dob="
+        + dob + ", occupation=" + occupation + ", employee=" + employee + "]";
+  }
 
 }
