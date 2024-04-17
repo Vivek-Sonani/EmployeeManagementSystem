886eb5a2e8cdbe53b04f89b1091247c93140a8de
Vivek Sonani <viveks@codetailor.in>
2024-04-17 17:39:32 +0530

    Change

--- src/main/java/com/springboot/employee/service/EmployeeService.java
+++ src/main/java/com/springboot/employee/service/EmployeeService.java
@@ -7,26 +7,26 @@ import com.springboot.employee.model.Employee;
 
 public interface EmployeeService {
 
-	Employee getEmployee(Integer id);
+  Employee getEmployee(Integer id);
 
-	List<Employee> getAllEmployee();
+  List<Employee> getAllEmployee();
 
-	Employee addEmployee(Employee employee);
+  Employee addEmployee(Employee employee);
 
-	void deleteEmployee(Integer id);
+  void deleteEmployee(Integer id);
 
-	Employee updateEmployee(Employee employee);
+  Employee updateEmployee(Employee employee);
 
-	void deleteEmployeesByPosition(String position);
+  void deleteEmployeesByPosition(String position);
 
-	List<Employee> getEmployeeByFirstName(String firstname);
+  List<Employee> getEmployeeByFirstName(String firstname);
 
-	List<Employee> FindAllByPage(int pageNo, int pageSize);
+  List<Employee> FindAllByPage(int pageNo, int pageSize);
 
-	List<Employee> findByName(String firstname);
+  List<Employee> findByName(String firstname);
 
-	List<Employee> findByPosition(String position);
+  List<Employee> findByPosition(String position);
 
-	Employee patch(String id, Map<String, Object> employee);
+  Employee patch(String id, Map<String, Object> employee);
 
 }
