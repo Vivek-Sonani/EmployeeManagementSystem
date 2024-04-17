d7aa7f9be444dd22f079e2ddbe9369123ee59001
Vivek Sonani <viveks@codetailor.in>
2024-04-17 17:52:07 +0530

    Master commit

--- src/main/java/com/springboot/employee/specification/EmployeeSpecification.java
+++ src/main/java/com/springboot/employee/specification/EmployeeSpecification.java
@@ -8,18 +8,19 @@ import com.springboot.employee.model.Employee_;
 
 public class EmployeeSpecification {
 
-	public static Specification<Employee> hasFirstname(String firstname) {
-		return ((root, Query, builder) -> {
-			 Query.distinct(true);
-			System.out.println("running.......");// distinct value we get
-			return builder.equal(root.get(Employee_.firstname), firstname);
-		});
-	}
-	public static Specification<Employee> hasPosition(String position) {
-		return ((root, CriteriaQuery, CriteriaBuilder) -> {
-			CriteriaQuery.distinct(true);
-			System.out.println("position");
-			return CriteriaBuilder.equal(root.get(Employee_.position), position);
-		});
-	}
+  public static Specification<Employee> hasFirstname(String firstname) {
+    return ((root, Query, builder) -> {
+      Query.distinct(true);
+      System.out.println("running.......");// distinct value we get
+      return builder.equal(root.get(Employee_.firstname), firstname);
+    });
+  }
+
+  public static Specification<Employee> hasPosition(String position) {
+    return ((root, CriteriaQuery, CriteriaBuilder) -> {
+      CriteriaQuery.distinct(true);
+      System.out.println("position");
+      return CriteriaBuilder.equal(root.get(Employee_.position), position);
+    });
+  }
 }
