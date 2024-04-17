7cf412277a92cdf72219b734219d3b690b4fdc69
Vivek Sonani <viveks@codetailor.in>
2024-04-17 12:23:55 +0530

    changes

--- src/main/java/com/springboot/employee/model/Employee.java
+++ src/main/java/com/springboot/employee/model/Employee.java
@@ -26,181 +26,182 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-@EntityListeners(value = { AuditListner.class, AuditingEntityListener.class })
+@EntityListeners(value = {AuditListner.class, AuditingEntityListener.class})
 @Table(name = "employee")
 public class Employee {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	Integer employId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Integer employId;
 
-	@Column
-	String firstname;
+  @Column
+  String firstname;
 
-	@Column
-	String middlename;
+  @Column
+  String middlename;
 
-	@Column
-	String lastname;
+  @Column
+  String lastname;
 
-	@Column
-	// @Length
-	String email;
+  @Column
+  // @Length
+  String email;
 
-	@Column
-	String position;
+  @Column
+  String position;
 
-	@Column
-	int contactNo;
+  @Column
+  int contactNo;
 
-	@Column
-	Float salary;
+  @Column
+  Float salary;
 
-	@Column(name = "created_date", nullable = false, updatable = false)
-	@CreatedDate
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date createdDate;
+  @Column(name = "created_date", nullable = false, updatable = false)
+  @CreatedDate
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
 
-	@Column(name = "modified_date", nullable = false)
-	@LastModifiedDate
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date modifiedDate;
+  @Column(name = "modified_date", nullable = false)
+  @LastModifiedDate
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date modifiedDate;
 
-	@Column(name = "created_by")
-	@CreatedBy
-	private String createdBy;
+  @Column(name = "created_by")
+  @CreatedBy
+  private String createdBy;
 
-	@Column(name = "modified_by")
-	@LastModifiedBy
-	private String modifiedBy;
+  @Column(name = "modified_by")
+  @LastModifiedBy
+  private String modifiedBy;
 
-	@OneToOne(mappedBy = "employee", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
-	private Address address;
+  @OneToOne(mappedBy = "employee", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+  private Address address;
 
-	@JsonManagedReference
-	@OneToMany(mappedBy = "employee", fetch = FetchType.LAZY)
-	private List<FamilyInfo> familyInfo;
+  @JsonManagedReference
+  @OneToMany(mappedBy = "employee", fetch = FetchType.LAZY)
+  private List<FamilyInfo> familyInfo;
 
-	public List<FamilyInfo> getFamilyInfo() {
-		return familyInfo;
-	}
+  public List<FamilyInfo> getFamilyInfo() {
+    return familyInfo;
+  }
 
-	public void setFamilyInfo(List<FamilyInfo> familyInfo) {
-		this.familyInfo = familyInfo;
-	}
+  public void setFamilyInfo(List<FamilyInfo> familyInfo) {
+    this.familyInfo = familyInfo;
+  }
 
-	public Address getAddress() {
-		return address;
-	}
+  public Address getAddress() {
+    return address;
+  }
 
-	public void setAddress(Address address) {
-		this.address = address;
-	}
+  public void setAddress(Address address) {
+    this.address = address;
+  }
 
-	public Employee() {
-		this.setEmployId((int) Math.random());
-	}
+  public Employee() {
+    this.setEmployId((int) Math.random());
+  }
 
-	public Integer getEmployId() {
-		return employId;
-	}
+  public Integer getEmployId() {
+    return employId;
+  }
 
-	public void setEmployId(Integer employId) {
-		this.employId = employId;
-	}
+  public void setEmployId(Integer employId) {
+    this.employId = employId;
+  }
 
-	public Float getSalary() {
-		return salary;
-	}
+  public Float getSalary() {
+    return salary;
+  }
 
-	public void setSalary(Float salary) {
-		this.salary = salary;
-	}
+  public void setSalary(Float salary) {
+    this.salary = salary;
+  }
 
-	public int getContactNo() {
-		return contactNo;
-	}
+  public int getContactNo() {
+    return contactNo;
+  }
 
-	public void setContactNo(int contactNo) {
-		this.contactNo = contactNo;
-	}
+  public void setContactNo(int contactNo) {
+    this.contactNo = contactNo;
+  }
 
-	public String getFirstname() {
-		return firstname;
-	}
+  public String getFirstname() {
+    return firstname;
+  }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
 
-	public String getMiddlename() {
-		return middlename;
-	}
+  public String getMiddlename() {
+    return middlename;
+  }
 
-	public void setMiddlename(String middlename) {
-		this.middlename = middlename;
-	}
+  public void setMiddlename(String middlename) {
+    this.middlename = middlename;
+  }
 
-	public String getLastname() {
-		return lastname;
-	}
+  public String getLastname() {
+    return lastname;
+  }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
 
-	public String getEmail() {
-		return email;
-	}
+  public String getEmail() {
+    return email;
+  }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-	public String getPosition() {
-		return position;
-	}
+  public String getPosition() {
+    return position;
+  }
 
-	public void setPosition(String position) {
-		this.position = position;
-	}
+  public void setPosition(String position) {
+    this.position = position;
+  }
 
-	public Date getCreatedDate() {
-		return createdDate;
-	}
+  public Date getCreatedDate() {
+    return createdDate;
+  }
 
-	public Date getModifiedDate() {
-		return modifiedDate;
-	}
+  public Date getModifiedDate() {
+    return modifiedDate;
+  }
 
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
 
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
+  public void setModifiedDate(Date modifiedDate) {
+    this.modifiedDate = modifiedDate;
+  }
 
-	public String getCreatedBy() {
-		return createdBy;
-	}
+  public String getCreatedBy() {
+    return createdBy;
+  }
 
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
+  public void setCreatedBy(String createdBy) {
+    this.createdBy = createdBy;
+  }
 
-	public String getModifiedBy() {
-		return modifiedBy;
-	}
+  public String getModifiedBy() {
+    return modifiedBy;
+  }
 
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
+  public void setModifiedBy(String modifiedBy) {
+    this.modifiedBy = modifiedBy;
+  }
 
-	@Override
-	public String toString() {
-		return "Employee [employId=" + employId + ", firstname=" + firstname + ", middlename=" + middlename
-				+ ", lastname=" + lastname + ", email=" + email + ", position=" + position + ", contactNo=" + contactNo
-				+ ", salary=" + salary + ", createdBy=" + createdBy + ", modifiedBy=" + modifiedBy + "]";
-	}
+  @Override
+  public String toString() {
+    return "Employee [employId=" + employId + ", firstname=" + firstname + ", middlename="
+        + middlename + ", lastname=" + lastname + ", email=" + email + ", position=" + position
+        + ", contactNo=" + contactNo + ", salary=" + salary + ", createdBy=" + createdBy
+        + ", modifiedBy=" + modifiedBy + "]";
+  }
 
 }
