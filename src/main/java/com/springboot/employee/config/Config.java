7fa345747c53dd95615b33a4f20a950b2876220a
Vivek Sonani <viveks@codetailor.in>
2024-04-17 11:39:08 +0530

    Fix: update indentation

--- src/main/java/com/springboot/employee/config/Config.java
+++ src/main/java/com/springboot/employee/config/Config.java
@@ -1,27 +1,27 @@
-//package com.springboot.employee.config;
+// package com.springboot.employee.config;
 //
-//import org.elasticsearch.client.RestHighLevelClient;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.elasticsearch.client.ClientConfiguration;
-//import org.springframework.data.elasticsearch.client.erhlc.AbstractElasticsearchConfiguration;
-//import org.springframework.data.elasticsearch.client.erhlc.RestClients;
-//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+// import org.elasticsearch.client.RestHighLevelClient;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.elasticsearch.client.ClientConfiguration;
+// import org.springframework.data.elasticsearch.client.erhlc.AbstractElasticsearchConfiguration;
+// import org.springframework.data.elasticsearch.client.erhlc.RestClients;
+// import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 //
-//@Configuration
-//@EnableElasticsearchRepositories(basePackages = "com.baeldung.spring.data.es.repository")
-//@ComponentScan(basePackages = { "com.baeldung.spring.data.es.service" })
-//public class Config extends AbstractElasticsearchConfiguration {
+// @Configuration
+// @EnableElasticsearchRepositories(basePackages = "com.baeldung.spring.data.es.repository")
+// @ComponentScan(basePackages = { "com.baeldung.spring.data.es.service" })
+// public class Config extends AbstractElasticsearchConfiguration {
 //
-//    @Bean
-//    @Override
-//    public RestHighLevelClient elasticsearchClient() {
-//        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-//            .connectedTo("localhost:9200")
-//            .build();
+// @Bean
+// @Override
+// public RestHighLevelClient elasticsearchClient() {
+// ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+// .connectedTo("localhost:9200")
+// .build();
 //
-//        return RestClients.create(clientConfiguration)
-//            .rest();
-//    }
-//}
+// return RestClients.create(clientConfiguration)
+// .rest();
+// }
+// }
