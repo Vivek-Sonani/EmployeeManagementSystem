fda0f14d271bb8d6c6dc60a82ee8575d03f98dc4
Vivek Sonani <viveks@codetailor.in>
2024-04-17 17:40:12 +0530

    Changes

--- src/main/java/com/springboot/employee/service/FamilyInfoServiceImp.java
+++ src/main/java/com/springboot/employee/service/FamilyInfoServiceImp.java
@@ -21,80 +21,82 @@ import com.springboot.employee.repository.FamilyInfoRepository;
 @Transactional(propagation = Propagation.MANDATORY)
 public class FamilyInfoServiceImp implements FamilyInfoService {
 
-	@Autowired
-	private FamilyInfoRepository familyInfoRepository;
+  @Autowired
+  private FamilyInfoRepository familyInfoRepository;
 
-	@Autowired
-	private EmployeeRepository employeeRepository;
+  @Autowired
+  private EmployeeRepository employeeRepository;
 
-	@Autowired
-	private DozerBeanMapper dozerBeanMapper;
+  @Autowired
+  private DozerBeanMapper dozerBeanMapper;
 
-	@Override
-	public FamilyInfo addFamilyInfo(FamilyInfo familyInfo) {
-		// TODO Auto-generated method stub
-		System.out.println("Implementation starting..");
-		Employee e = familyInfo.getEmployee();
-		System.out.println(e);
-		Employee e1 = employeeRepository.findById(e.getEmployId()).get();
-		System.out.println(e1);
-		familyInfo.setEmployee(e1);
-		System.out.println(familyInfo);
-		// Optional<Employee> e1=employeeRepository.findById(e.getEmployId());
-		FamilyInfo f = familyInfoRepository.save(familyInfo);
-		System.out.println("saved successfully....");
-		return dozerBeanMapper.map(f, FamilyInfo.class, "family-info-basic");
-	}
+  @Override
+  public FamilyInfo addFamilyInfo(FamilyInfo familyInfo) {
+    // TODO Auto-generated method stub
+    System.out.println("Implementation starting..");
+    Employee e = familyInfo.getEmployee();
+    System.out.println(e);
+    Employee e1 = employeeRepository.findById(e.getEmployId()).get();
+    System.out.println(e1);
+    familyInfo.setEmployee(e1);
+    System.out.println(familyInfo);
+    // Optional<Employee> e1=employeeRepository.findById(e.getEmployId());
+    FamilyInfo f = familyInfoRepository.save(familyInfo);
+    System.out.println("saved successfully....");
+    return dozerBeanMapper.map(f, FamilyInfo.class, "family-info-basic");
+  }
 
-	@Override
-	public FamilyInfo updateFamilyInfo(FamilyInfo familyInfo) {
-		// TODO Auto-generated method stub
-		Optional<FamilyInfo> familyOptional = familyInfoRepository.findById(familyInfo.getId());
-		if (!familyOptional.isPresent()) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-		} else {
-			FamilyInfo f = familyOptional.get();
-			f.setName(familyInfo.getName());
-			f.setDob(familyInfo.getDob());
-			f.setOccupation(familyInfo.getOccupation());
-			f.setRelationship(familyInfo.getRelationship());
+  @Override
+  public FamilyInfo updateFamilyInfo(FamilyInfo familyInfo) {
+    // TODO Auto-generated method stub
+    Optional<FamilyInfo> familyOptional = familyInfoRepository.findById(familyInfo.getId());
+    if (!familyOptional.isPresent()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    } else {
+      FamilyInfo f = familyOptional.get();
+      f.setName(familyInfo.getName());
+      f.setDob(familyInfo.getDob());
+      f.setOccupation(familyInfo.getOccupation());
+      f.setRelationship(familyInfo.getRelationship());
 
-			FamilyInfo family = familyInfoRepository.save(f);
-			System.out.println("Updated successfully....");
-			return dozerBeanMapper.map(family, FamilyInfo.class, "family-info-basic");
+      FamilyInfo family = familyInfoRepository.save(f);
+      System.out.println("Updated successfully....");
+      return dozerBeanMapper.map(family, FamilyInfo.class, "family-info-basic");
 
-		}
+    }
 
-	}
+  }
 
-	@Override
-	public FamilyInfo getFamilyInfo(Integer id) {
-		// TODO Auto-generated method stub
-		Optional<FamilyInfo> f = familyInfoRepository.findById(id);
-		if (f.isPresent()) {
-			FamilyInfo familyInfo = f.get();
-			return familyInfo;
-		} else {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-		}
-	}
+  @Override
+  public FamilyInfo getFamilyInfo(Integer id) {
+    // TODO Auto-generated method stub
+    Optional<FamilyInfo> f = familyInfoRepository.findById(id);
+    if (f.isPresent()) {
+      FamilyInfo familyInfo = f.get();
+      return familyInfo;
+    } else {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    }
+  }
 
-	@Override
-	public List<FamilyInfo> getByEmployeeId(Integer employeeId) {
-		// TODO Auto-generated method stub
-		List<FamilyInfo> familyList = familyInfoRepository.findByEmployeeEmployId(employeeId);
-		// System.out.println(e);
-		// familyInfoRepository.findAllById(familyInfo.getId());
-		System.out.println(familyList);
-		return familyList.stream().map(e -> dozerBeanMapper.map(e, FamilyInfo.class,"family-info-basic")).collect(Collectors.toList());
-	}
+  @Override
+  public List<FamilyInfo> getByEmployeeId(Integer employeeId) {
+    // TODO Auto-generated method stub
+    List<FamilyInfo> familyList = familyInfoRepository.findByEmployeeEmployId(employeeId);
+    // System.out.println(e);
+    // familyInfoRepository.findAllById(familyInfo.getId());
+    System.out.println(familyList);
+    return familyList.stream()
+        .map(e -> dozerBeanMapper.map(e, FamilyInfo.class, "family-info-basic"))
+        .collect(Collectors.toList());
+  }
 
-	@Override
-	public void removeFamilyInfo(Integer id) {
-		// TODO Auto-generated method stub
-		familyInfoRepository.deleteById(id);
-		System.out.println("deleted successfully...");
+  @Override
+  public void removeFamilyInfo(Integer id) {
+    // TODO Auto-generated method stub
+    familyInfoRepository.deleteById(id);
+    System.out.println("deleted successfully...");
 
-	}
+  }
 
 }
