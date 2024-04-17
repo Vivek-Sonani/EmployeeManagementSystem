399164011ced622ebd15a6380f1471d9b999c293
Vivek Sonani <viveks@codetailor.in>
2024-04-17 17:38:27 +0530

    changes

--- src/main/java/com/springboot/employee/service/AddressService.java
+++ src/main/java/com/springboot/employee/service/AddressService.java
@@ -4,10 +4,13 @@ import com.springboot.employee.model.Address;
 
 public interface AddressService {
 
-	public Address addAddress(Address address);
-	public Address updateAddress(Address address);
-	public void removeAddress(Address dbAddress);
-	//Address updateAddress(Address address, Employee emp);
-	public Address getAddress(Address address);
+  public Address addAddress(Address address);
+
+  public Address updateAddress(Address address);
+
+  public void removeAddress(Address dbAddress);
+
+  // Address updateAddress(Address address, Employee emp);
+  public Address getAddress(Address address);
 
 }
