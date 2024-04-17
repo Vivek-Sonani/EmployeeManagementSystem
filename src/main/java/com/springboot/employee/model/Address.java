8b7daa4739edbb7589e69f652619693bec40b047
Vivek Sonani <viveks@codetailor.in>
2024-04-17 12:23:24 +0530

    Fix:

--- src/main/java/com/springboot/employee/model/Address.java
+++ src/main/java/com/springboot/employee/model/Address.java
@@ -16,72 +16,73 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@EntityListeners(value = { AuditListner.class, AuditingEntityListener.class })
+@EntityListeners(value = {AuditListner.class, AuditingEntityListener.class})
 @Table(name = "address")
 public class Address {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 
-	@Column
-	private String city;
+  @Column
+  private String city;
 
-	@Column
-	private String state;
+  @Column
+  private String state;
 
-	@OneToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "employee_id")
-	@JsonIgnore
-	private Employee employee;
+  @OneToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "employee_id")
+  @JsonIgnore
+  private Employee employee;
 
-	public Employee getEmployee() {
-		return employee;
-	}
+  public Employee getEmployee() {
+    return employee;
+  }
 
-	public void setEmployee(Employee employee) {
-		this.employee = employee;
-	}
+  public void setEmployee(Employee employee) {
+    this.employee = employee;
+  }
 
-	public Address() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+  public Address() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
 
-	public Address(Integer id, String city, String state) {
-		super();
-		this.id = id;
-		this.city = city;
-		this.state = state;
-	}
+  public Address(Integer id, String city, String state) {
+    super();
+    this.id = id;
+    this.city = city;
+    this.state = state;
+  }
 
-	public Integer getId() {
-		return id;
-	}
+  public Integer getId() {
+    return id;
+  }
 
-	public String getCity() {
-		return city;
-	}
+  public String getCity() {
+    return city;
+  }
 
-	public String getState() {
-		return state;
-	}
+  public String getState() {
+    return state;
+  }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+  public void setCity(String city) {
+    this.city = city;
+  }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+  public void setState(String state) {
+    this.state = state;
+  }
 
-	@Override
-	public String toString() {
-		return "Address [id=" + id + ", city=" + city + ", state=" + state + ", employee=" + employee + "]";
-	}
+  @Override
+  public String toString() {
+    return "Address [id=" + id + ", city=" + city + ", state=" + state + ", employee=" + employee
+        + "]";
+  }
 
 }
